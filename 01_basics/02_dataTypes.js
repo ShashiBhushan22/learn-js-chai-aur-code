007877e4fd9ed3af4a95503e78d491867236b34c
Shashi bhushan Jha <90974208+ShashiBhushan22@users.noreply.github.com>
2023-09-26 13:44:53 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not brower
+
+console.log(3 +3); //code readablity should be high priority
+
+let name = "Shashi"
+
+let age = 25
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object 
+
+console.log(typeof"Hello");
+console.log(typeof null);
+console.log(typeof undefined);
